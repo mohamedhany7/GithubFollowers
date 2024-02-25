a67bfd2c3bbe63e3bb2f8afdbbdfa6517998566f
mohamedhany7 <mohameddhanyy7@gmail.com>
2024-02-25 02:32:16 +0200

    Refactoring into <Result>

--- Github Followers/Controllers/FollowerListVC.swift
+++ Github Followers/Controllers/FollowerListVC.swift
@@ -16,19 +16,22 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(alertTitle: "Error", messageTitle: errorMessage!.rawValue, buttonTitle: "OK")
-                return
-            }
-            
-            print("Followers count = \(followers.count)")
-            print(followers)
-        }
+        fetchUsers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func fetchUsers(){
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(alertTitle: "Error", messageTitle: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
 }
