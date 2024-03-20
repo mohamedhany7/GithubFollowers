235cf18b05bdf5c85f42d2f5926decc49cfe74bb
mohamedhany7 <mohameddhanyy7@gmail.com>
2024-03-20 22:17:29 +0200

    downloading avatar images from url

--- Github Followers/Views/GFAvatarImageView.swift
+++ Github Followers/Views/GFAvatarImageView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class GFAvatarImageView: UIImageView {
+    let placeholderAvatar = UIImage(named: K.avatar)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,6 +22,25 @@ class GFAvatarImageView: UIImageView {
     private func configure(){
         layer.cornerRadius = 10
         clipsToBounds = true
+        image = placeholderAvatar
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func downloadImage(from urlString: String){
+        guard let url = URL(string: urlString) else { return }
         
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        
+        task.resume()
     }
 }
