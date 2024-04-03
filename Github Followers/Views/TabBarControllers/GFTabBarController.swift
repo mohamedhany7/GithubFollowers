efdf4b83357befa775fe8525230fa0dba31169f3
mohamedhany7 <mohameddhanyy7@gmail.com>
2024-04-03 18:20:53 +0200

    Refactored scene delegate into tabBarViewController

---
+++ Github Followers/Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  GFTabBarController.swift
+//  Github Followers
+//
+//  Created by Mohamed Hany on 03/04/2024.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController{
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController{
+        let favoritesVC = FavoritesListVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+    
+    func createTabBar()-> UITabBarController {
+        let tabBar = UITabBarController()
+        
+        return tabBar
+    }
+}
